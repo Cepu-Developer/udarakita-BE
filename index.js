386c1ebbc0d7c8a0148ce6de9f90bb218ad652d0
Muhammad Ariansyah <115616443+AriiAnn@users.noreply.github.com>
2023-12-15 22:57:55 +0700

    update

--- index.js
+++ index.js
@@ -30,4 +30,5 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 
-app.listen(5000, () => console.log("Server running at port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
